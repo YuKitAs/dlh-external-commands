6ed85ab88b952679c5c5f27cfc1a05f6914c426e
xiGUAwanOU <xiguawanou@gmail.com>
2017-04-03 01:36:56 +0200

    Added a docstring for GoogleTimer command.

--- GoogleTimer.py
+++ GoogleTimer.py
@@ -6,6 +6,7 @@ tags = ['timer', 'countdown', 'clock']
 
 
 def do(self, line):
+    """Set a timer using Google Search."""
     time_format = re.compile(r'\d+[HhMmSs]')
     if time_format.match(line.strip()):
         subprocess.call('open https://www.google.de/#q=timer+%s' % line, shell=True)
