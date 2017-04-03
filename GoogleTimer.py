6a77ccc52c484d96ac926df60ee4134052050b49
Zihan Lin <zihanlin@flaregames.com>
2017-04-03 13:50:39 +0200

    Strip arguments before processing them.

--- GoogleTimer.py
+++ GoogleTimer.py
@@ -10,8 +10,9 @@ BASE_URL = 'https://www.google.de/#q=timer+'
 
 def do(self, line):
     """Set a timer using Google Search."""
+    line = line.strip()
     time_format = re.compile(r'\d+[HhMmSs]')
-    if time_format.match(line.strip()):
+    if time_format.match(line):
         subprocess.call([BROWSER_COMMAND, '%s%s' % (BASE_URL, line)])
     else:
         print('Please specify time using pattern: "\d+[HhMmSs]"')
